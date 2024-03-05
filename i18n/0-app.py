1ba1417879c883410eaf7834597373532de0e632
misskukuua <p.asare@alustudent.com>
2024-03-05 18:02:26 +0200

    addded documentation

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
-# import flask module
+"""
+This is a basic Flask application that returns "Hello world" when accessing the root URL ('/').
+"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-# define the app route
+"""define the app route"""
 @app.route('/')
 def index():
     return render_template('0-index.html')
 
-# run the flask app
+"""run the flask app"""
 if __name__ == '__main__':
     app.run(debug=True)
