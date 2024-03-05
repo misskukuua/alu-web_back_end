3d23694eb89702ba109ba5115ab1b472c7f15fe4
misskukuua <p.asare@alustudent.com>
2024-03-05 18:00:40 +0200

    added comments

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-
+# import flask module
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
+# define the app route
 @app.route('/')
 def index():
     return render_template('0-index.html')
 
-
+# run the flask app
 if __name__ == '__main__':
     app.run(debug=True)
