522206f4fa1c551f91345afae0e9d61554f2eefc
misskukuua <p.asare@alustudent.com>
2024-03-05 18:07:41 +0200

    fixed errors

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -6,11 +6,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 """define the app route"""
 @app.route('/')
 def index():
     return render_template('0-index.html')
 
-# run the flask app
+
+"""run the flask app"""
+
+
 if __name__ == '__main__':
     app.run(debug=True)
