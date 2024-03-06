9a84c3b3b6e63de13c3e6efa472843bb179a6ef2
misskukuua <p.asare@alustudent.com>
2024-03-06 19:54:32 +0200

    Fixed language

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -36,8 +36,7 @@ def get_locale():
     Determine the best match with supported languages
     based on request.accept_languages.
     """
-    return request.accept_languages.best_match
-(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
