c9805345f099c62123eaebd07e513340101c5774
misskukuua <p.asare@alustudent.com>
2024-03-06 19:25:07 +0200

    basic babel app

---
+++ i18n/1-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from flask import Config, Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+#Store babel object in a variable named babel.
+babel = Babel(app)
+
+
+"""" create a Config class with
+     the specified attributes.
+"""
+
+
+class config:
+# Define available languages
+    LANGUAGES = ['en', 'fr']
+     # Set default locale and timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
+
+"""run the flask app"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
