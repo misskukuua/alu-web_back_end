0d4787b75ca800e916d4986238bf12f65e613701
misskukuua <p.asare@alustudent.com>
2024-03-25 17:54:50 +0200

    all

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
   get sqft() {
     return this._sqft;
   }
-
-  // Abstract method
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
-
-export default Building;
