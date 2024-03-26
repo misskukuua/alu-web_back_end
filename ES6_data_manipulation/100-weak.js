1c433d51f9214e52f9fb5a3d45f54968bba9ea96
misskukuua <p.asare@alustudent.com>
2024-03-26 16:34:01 +0200

    weak

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   let count = weakMap.get(endpoint) || 0;
-  count++;
+  count += 1;
   weakMap.set(endpoint, count);
 
   if (count >= 5) {
