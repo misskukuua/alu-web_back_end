2a6cb9c1d7a37c4e58528b43cb7d968831c33c43
misskukuua <p.asare@alustudent.com>
2024-04-04 18:10:54 +0200

    updated testfile

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,22 @@
-function calculateNumber(a, b) {
+function calculateNumber(operation, a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
-  return roundedA + roundedB;
+  
+  switch (operation) {
+      case 'SUM':
+          return roundedA + roundedB;
+      case 'SUBTRACT':
+          return roundedA - roundedB;
+      case 'DIVIDE':
+          if (roundedB === 0) {
+              return 'Error';
+          }
+          return roundedA / roundedB;
+      case 'SECOND_NUMBER_ROUNDED':
+          return roundedB;
+      default:
+          throw new Error('Invalid operation');
+  }
 }
 
 module.exports = calculateNumber;
--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,15 +4,29 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function() {
     it('should return the sum of rounded numbers', function() {
         // Test case 1
-        assert.equal(calculateNumber(1.4, 2.6), 4);
+        assert.equal(calculateNumber('SUM', 1.4, 2.6), 4);
         
         // Test case 2
-        assert.equal(calculateNumber(2.1, 3.8), 6);
+        assert.equal(calculateNumber('SUM', 2.1, 3.8), 6);
         
         // Test case 3
-        assert.equal(calculateNumber(1.49, 2.51), 4);
+        assert.equal(calculateNumber('SUM', 1.49, 2.51), 4);
         
         // Test case 4
-        assert.equal(calculateNumber(0, 0), 0);
+        assert.equal(calculateNumber('SUM', 0, 0), 0);
+    });
+
+    it('should return the second number rounded', function() {
+        // Test case 1
+        assert.equal(calculateNumber('SECOND_NUMBER_ROUNDED', 1.4, 4.5), 5);
+        
+        // Test case 2
+        assert.equal(calculateNumber('SECOND_NUMBER_ROUNDED', 2.1, 3.8), 4);
+        
+        // Test case 3
+        assert.equal(calculateNumber('SECOND_NUMBER_ROUNDED', 1.49, 2.51), 3);
+        
+        // Test case 4
+        assert.equal(calculateNumber('SECOND_NUMBER_ROUNDED', 0, 0), 0);
     });
 });
