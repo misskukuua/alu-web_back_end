ff72cac214ccdb2883fd8aa5f1c30fbda15d2e3d
misskukuua <p.asare@alustudent.com>
2024-04-05 11:32:52 +0200

    node_modules.

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+function displayMessage(message) {
+    // Print the message to standard output (STDOUT)
+    console.log(message);
+}
+
+// Export the displayMessage function
+module.exports = displayMessage;
