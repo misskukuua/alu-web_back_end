34a93801ed627dd470212b0de6e05718e269b475
misskukuua <p.asare@alustudent.com>
2024-04-12 13:01:57 +0200

    updated file

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,4 @@
-function displayMessage(message) {
-    // Print the message to standard output (STDOUT)
-    console.log(message);
-}
-
-// Export the displayMessage function
-module.exports = displayMessage;
+module.exports = function displayMessage(stdout) {
+    console.log(stdout);
+  };
+  
